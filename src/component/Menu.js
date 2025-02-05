10e2292029f492dff40766d7f0b26999bc827bb8
yasinthon18 <s6603052423031@email.kmutnb.ac.th>
2025-02-05 15:56:02 +0700

    add file Menu.js Quiz.js Score.js and edit add folder data and add file QuestionData.js and edit
    add file Menu.js Quiz.js Score.js and edit add folder data and add file QuestionData.js and edit

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { DataContext } from "../App";
+
 const Menu = ()=>{
+    const {setAppState} = useContext(DataContext)
     return(
-        <div clasName ="menu">
+        <div className ="menu">
             <h1>Menu Component</h1>
+            <button onClick={()=>setAppState("quiz")}>เริ่มทำแบบข้อสอบ</button>
         </div>
     )
 }
--- src/component/Score.js
+++ src/component/Score.js
@@ -1,7 +1,19 @@
+import { useContext } from "react";
+import { DataContext } from "../App";
+import QuestionsData from "../data/QuestionData";
+
 const Score = ()=>{
+    const {score,setAppState,setScore} = useContext(DataContext)
+
+    const restartApp=()=>{
+        setScore(0)
+        setAppState("menu")
+    }
     return(
-        <div clasName ="score">
-            <h1>Score Component</h1>
+        <div className ="score">
+            <h1>สรุปผลคะแนน</h1>
+            <h2>{score} / {QuestionsData.length}</h2>
+            <button onClick={restartApp}>ทำแบบทดสอบอีกครั้ง</button>
         </div>
     )
 }
